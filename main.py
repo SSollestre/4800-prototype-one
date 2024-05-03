faf404c7ceedca2c651d7fb4d56800e3a0d85e21
SSollestre <sean.sollestre@gmail.com>
2024-05-03 00:55:11 -0700

    Modify cors

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Allow requests from all origins (change '*' to a list of allowed origins if desired)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 async def root():
